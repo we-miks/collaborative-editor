3e2516df9445a8040b0293d099279affd251f9df
Jeremy <renjie@yuanben.org>
2020-11-06 17:37:39 +0800

    merge display and index

--- demo/index.js
+++ demo/index.js
@@ -3,6 +3,15 @@ import 'quill/dist/quill.snow.css'
 import EditorEvents from "../editor-events";
 import '../modules/task-list';
 
+import ReconnectingWebSocket from "reconnecting-websocket";
+import ShareDB from "sharedb/lib/client";
+import Quill from 'quill';
+import richText from "rich-text";
+ShareDB.types.register(richText.type);
+
+import '../display.styl';
+
+
 let authors = [
     {
         id: 1,
@@ -113,6 +122,43 @@ editor.on(EditorEvents.synchronizationError, (err) => {
     console.log(err);
 });
 
+
+
 let websocketEndpoint = "ws://127.0.0.1:8080";
 
 editor.syncThroughWebsocket(websocketEndpoint, "examples", "test-doc");
+
+let socket = new ReconnectingWebSocket(websocketEndpoint);
+
+let connection = new ShareDB.Connection(socket);
+
+let doc = connection.get("examples", "test-doc");
+
+// Create a hidden quill editor to parse delta to html
+
+
+let editorContainer = document.createElement('div');
+editorContainer.style.display = 'none';
+
+let quill = new Quill(editorContainer);
+
+doc.fetch((err) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+
+    let delta = doc.data;
+    quill.setContents(delta);
+
+    document.querySelector(".content").innerHTML = quill.root.innerHTML;
+
+    doc.destroy();
+    socket.close();
+
+    editor.on(EditorEvents.editorTextChanged, (delta) => {
+        let del = delta.oldDelta.compose(delta.delta)
+        quill.setContents(del);
+        document.querySelector(".content").innerHTML = quill.root.innerHTML;
+    })
+});
